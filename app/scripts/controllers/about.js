4a4551300357fba00a1421c9e1c06afb716fcc6a
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2014-07-29 09:40:25 +0900

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name metroHomepageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the metroHomepageApp
+ */
+angular.module('metroHomepageApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
