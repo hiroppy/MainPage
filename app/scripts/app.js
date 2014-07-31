e036c6fde0adb7f61a063df3b577a5eebcc52462
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2014-07-31 10:45:20 +0900

    html5Mode change true

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,11 +17,18 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
+  .config(["$locationProvider", function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+  }])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/individualTemplete', {
+        templateUrl: 'views/individualTemplete.html',
+        controller: 'IndividualTempleteCtrl'
       })
       .when('/accounts', {
         templateUrl: 'views/accounts.html',
