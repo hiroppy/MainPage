8436e5774e72648b6b16a7c9f0ad3bc5023c3268
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2014-08-05 21:27:39 +0900

    wip

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name metroHomepageApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the metroHomepageApp
- */
 angular.module('metroHomepageApp')
   .controller('MainCtrl', function ($scope) {
 
+    //android-explode
+    $('div#explode-tile').click(function(event){
+      // console.log(event.target);
+      $('.tile').animate({
+        opacity: 0
+      },1200);
+    });
   });
