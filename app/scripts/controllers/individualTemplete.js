0ad722945ce586cd05120fe89dbbcfcc363e3d86
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2014-08-06 21:05:26 +0900

    add page-reload

--- app/scripts/controllers/individualTemplete.js
+++ app/scripts/controllers/individualTemplete.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('metroHomepageApp')
-  .controller('IndividualTempleteCtrl', function ($scope,$location) {
+  .controller('IndividualTempleteCtrl', function ($scope,$location,$timeout) {
     $scope.location = $location.path().substr(1);
+
+    $('.page-reload').click(function(e){
+      location.reload();
+    });
+
+    // $scope.request = 0;
+    // $scope.response = 0;
+
+    // $timeout(function(){
+    //   var timing = performance.timing;
+    //   console.log(timing);
+    //   // console.log(timing.responseEnd +" "+ timing.responseStart);
+    //   // $scope.response = timing.responseEnd - timing.responseStart;
+    // },0);
   });
