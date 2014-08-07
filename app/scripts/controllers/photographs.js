84d116b7d4dd5a49d48b9fd75820690babacab84
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2014-08-07 10:44:54 +0900

    wip

--- app/scripts/controllers/photographs.js
+++ app/scripts/controllers/photographs.js
@@ -1,11 +1,60 @@
 'use strict';
 
+//issues
+//https://github.com/abouthiroppy/metroHomePage/issues/6
+
+
 angular.module('metroHomepageApp')
   .controller('PhotographsCtrl', function ($scope) {
-    console.log ('------in');
-
+    $('.tab-control').tabcontrol({
+      effect: 'fade' // or 'slide'
+    });
+    $.ajax({
+      async: true,
+      url: 'photographs-images/photo.json',
+      type: 'GET',
+      dataType: 'json'
+    }).done(function(data){
+      var imagesPathArray = [];
+      for(var i=0;i<data.length;i++){
+        data[i].name = data[i].name;
+        imagesPathArray.push(data[i]);
+        $('#all').append('<a rel=\"shadowbox\" href=\"photographs-images/'+data[i].name+'\" title=\"'+data[i].title+'\"><img alt=\"'+data[i].title+'\" src=\"photographs-images/'+data[i].name+'\"></a>');
+        if(data[i].type === '0')
+          $('#object').append('<a rel=\"shadowbox\" href=\"photographs-images/'+data[i].name+'\" title=\"'+data[i].title+'\"><img alt=\"'+data[i].title+'\" src=\"photographs-images/'+data[i].name+'\"></a>');
+        if(data[i].type === '1')
+          $('#cute').append('<a rel=\"shadowbox\" href=\"photographs-images/'+data[i].name+'\" title=\"'+data[i].title+'\"><img alt=\"'+data[i].title+'\" src=\"photographs-images/'+data[i].name+'\"></a>');
+      }
+      // console.log(imagesPathArray);
+      Shadowbox.init();
+    });
+    $scope.$on('$viewContentLoaded',function(){
+      setTimeout(function(){
+        $("#all").justifiedGallery({cssAnimation:true});
+        $("#object").justifiedGallery({cssAnimation:true});
+        $("#cute").justifiedGallery({cssAnimation:true});
+      },300);
+    });
   });
-
-$('.tab-control').tabcontrol({
-  effect: 'fade' // or 'slide'
-});
+  // .directive('shadowbox', function() {
+  //   console.log('in');
+  //   return {
+  //     // here you can style the link/thumbnail/etc.
+  //     template: '<a ng-click="openShadowbox()">{{imageName}}</a>',
+  //     scope: {
+  //       imageName: '@name',
+  //       imageUrl: '@url'
+  //     },
+  //     link: function (scope, element, attrs) {
+  //       scope.openShadowbox = function () {
+  //         Shadowbox.open({
+  //           content:    '<div id="welcome-msg">Welcome to my website!</div>',
+  //           player:     "html",
+  //           title:      "Welcome",
+  //           height:     350,
+  //           width:      350
+  //         });
+  //       };
+  //     }
+  //   }
+  // });
