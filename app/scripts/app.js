8e142f5925bc962762696c0461adb107b583588d
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2014-08-08 18:06:31 +0900

    remove commentout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name metroHomepageApp
- * @description
- * # metroHomepageApp
- *
- * Main module of the application.
- */
 angular
   .module('metroHomepageApp', [
     'ngAnimate',
