34e132e60ac2adbf3212a4942b02d860f2761233
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2014-09-24 18:54:46 +0900

    delete task when grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -431,7 +431,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build', [
         'clean:dist',
-        'wiredep',
+        // 'wiredep',
         'useminPrepare',
         'concurrent:dist',
         'autoprefixer',
